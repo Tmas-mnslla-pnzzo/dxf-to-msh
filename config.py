b4bc955bade47e5ce8891d262c2c5935bd7d8694
Tomas Mansilla Panozzo <98186505+Tmas-mnslla-pnzzo@users.noreply.github.com>
2025-02-05 21:56:56 -0300

    Create config.py
    Se añadio un archivo de configuración

---
+++ config.py
@@ -0,0 +1,12 @@
+prefijo_capa_agujeros = "int" 
+max_area = 100
+num_intermedios_linea = 10
+num_intermedios_circulo = 10
+num_intermedios_spline = 0.1
+presicion = 6
+indice_punto_contorno = 0
+indice_punto_agujero = 0
+op_vtk=False
+op_npz=True
+op_csv=True
+op_vis=True
