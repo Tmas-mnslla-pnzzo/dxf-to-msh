f4e449f802e4d10b737fbe0af215b001f2f8a988
Tomas Mansilla Panozzo <98186505+Tmas-mnslla-pnzzo@users.noreply.github.com>
2025-02-21 20:03:09 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,13 @@
 prefijo_capa_agujeros = "int" 
-max_area = 100
-num_intermedios_linea = 10
-num_intermedios_circulo = 10
+max_area = 0.01
+densidad=5
+num_intermedios_linea = 2
+num_intermedios_circulo = 20
 num_intermedios_spline = 0.1
 presicion = 6
 indice_punto_contorno = 0
 indice_punto_agujero = 0
+
 op_vtk=False
 op_npz=True
 op_csv=True
