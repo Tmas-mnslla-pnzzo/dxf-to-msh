d5e2b56d6314d96126231487e95e87513221da44
Tomas Mansilla Panozzo <98186505+Tmas-mnslla-pnzzo@users.noreply.github.com>
2025-02-21 20:04:01 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,16 @@ from math import cos, sin, pi
 from config import *
 from geometria import leer_entidades_dxf_por_capa, generar_malla_triangle, guardar_malla_vtk, guardar_malla_npz, guardar_malla_csv,  visualizar_malla
 
-ruta_dxf = input("Nombre del archivo con extensión dxf: ")
+if len(sys.argv) < 2:
+    print("Error: Debes proporcionar el nombre del archivo.")
+    sys.exit(1) 
+
+ruta = sys.argv[1]
+ruta_dxf = ruta + ".dxf"
 
 puntos_contorno, segmentos_contorno, conj_puntos_agujeros, conj_segmentos_agujeros, clasificaciones_contorno, clasificaciones_agujeros = leer_entidades_dxf_por_capa(
     ruta_dxf, prefijo_capa_agujeros, num_intermedios_linea, num_intermedios_circulo, 
-    num_intermedios_spline, indice_punto_contorno, indice_punto_agujero
+    num_intermedios_spline, indice_punto_contorno, indice_punto_agujero, densidad
 )
 
 malla = generar_malla_triangle(
@@ -20,10 +25,10 @@ malla = generar_malla_triangle(
 )
 
 if op_vtk:
-    guardar_malla_vtk(ruta_dxf, malla)
+    guardar_malla_vtk(ruta, malla)
 if op_npz:
-    guardar_malla_npz(ruta_dxf, malla)
+    guardar_malla_npz(ruta, malla)
 if op_csv:
-    guardar_malla_csv(ruta_dxf, malla)
+    guardar_malla_csv(ruta, malla)
 if op_vis:
     visualizar_malla(malla)
